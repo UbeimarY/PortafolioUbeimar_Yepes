f6c3457077eb4cb363366c5d3c6442159e9d4163
Juanp.lucero <ubeimar.yepespor@campusucc.edu.co>
2025-09-16 23:48:17 -0500

    Primer Commit, Portada

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 bg-black-500 be-red-500">
+      <h1 className="text-5xl font-bold text-white drop-shadow-lg">
+        Ubeimar Lizardo Yepes Portilla
+      </h1>
+    </main>
+  );
+}
